c55ccde10c229391cb8afb91baf8e351ef8de5c6
Dhickssoer <112595856+Dhickssoer@users.noreply.github.com>
2023-05-16 20:25:43 -0300

    Opção voltar ao menu ou permacer na tela
    Adicionado a opção voltar ao menu, ou repetir a ação para as telas de registro, consulta e deleção.

--- Cartorio.c
+++ Cartorio.c
@@ -15,119 +15,150 @@ int registro() // Função responsável por cadastrar usuários no sistema
 	char nome[40];
 	char sobrenome[40];
 	char cargo[40];
+	int selecao = 1;
 	//fim da criação das variáveis
 	
-	
-	printf("### CADASTRO ###\n\n\n");
-	
-	printf("Digite o CPF: ");
-	scanf("%s",cpf);
-	
-	strcpy(arquivo,cpf); // responsável por copiar os valores da strings
-	
-	FILE*file; // cria o arquivo
-	file =  fopen(arquivo,"w");// Cria um arquivo com o nome = ao conteúdo da String "arquivo"
-	fprintf(file,"CPF:"); //escreve dentro do arquivo o conteúdo que está entre aspas
-	fprintf(file,cpf); //escreve no arquivo o conteúdo da string "cpf"
-	fprintf(file,"| ");
-	fclose(file); // fecha o arquivo
-	
-	
-	printf("Digite o NOME: "); //coletando informação do usuário
-	scanf("%s",nome);//%s refere-se a strings
-	
-	file = fopen(arquivo,"a"); // abre o arquivo cujo nome está armazenado na variáveil "arquivo
-	fprintf(file,"Nome: "); // escreve o conteúdo das aspas dentro do arquivo
-	fprintf(file,nome); // escreve o conteúdo da variável nome dentro do arquivo
-	fprintf(file," ");// adiciona um espaça dentro arquivo
-	fclose(file); // fecha o arquivo
+	do{
+		system ("cls");
+		printf("### CADASTRO ###\n\n\n");
 		
-	printf("Digite o SOBRENOME: "); // coletando informação do usuário
-	scanf("%s",sobrenome); // armazena o conteúdo digitado dentro da string sobrenome
-	
-	file = fopen(arquivo,"a"); // abre o arquivo. O "a" significa que o arquivo será atualizado
-	fprintf(file,sobrenome); // escreve o conteúdo da variável sobrenome dentro do arquivo
-	fprintf(file,"| "); // escreve o conteúdo das aspas no arquivo
-	fclose(file); // fecha o arquivo
-	
-	printf("Digite o CARGO: "); //coletando informação do usuário
-	scanf("%s", cargo); // armazena o conteúdo digitado na string cargo
-	
-	file = fopen(arquivo,"a"); //abre o arquivo.
-	fprintf(file,"Cargo: "); //escreve o conteúdo das aspas dentro do arquivo
-	
-	fprintf(file,cargo); // escreve o conteúdo da variável cargo no arquivo
-	fclose(file); // fecha o arquivo
-	
-	printf("\n\n\n");
-	system("pause"); // pausa do sistema
-	
+		printf("Digite o CPF: ");
+		scanf("%s",cpf);
+		
+		strcpy(arquivo,cpf); // responsável por copiar os valores da strings
+		
+		FILE*file; // cria o arquivo
+		file =  fopen(arquivo,"w");// Cria um arquivo com o nome = ao conteúdo da String "arquivo"
+		fprintf(file,"CPF:"); //escreve dentro do arquivo o conteúdo que está entre aspas
+		fprintf(file,cpf); //escreve no arquivo o conteúdo da string "cpf"
+		fprintf(file,"| ");
+		fclose(file); // fecha o arquivo
+		
+		
+		printf("Digite o NOME: "); //coletando informação do usuário
+		scanf("%s",nome);//%s refere-se a strings
+		
+		file = fopen(arquivo,"a"); // abre o arquivo cujo nome está armazenado na variáveil "arquivo
+		fprintf(file,"Nome: "); // escreve o conteúdo das aspas dentro do arquivo
+		fprintf(file,nome); // escreve o conteúdo da variável nome dentro do arquivo
+		fprintf(file," ");// adiciona um espaça dentro arquivo
+		fclose(file); // fecha o arquivo
+			
+		printf("Digite o SOBRENOME: "); // coletando informação do usuário
+		scanf("%s",sobrenome); // armazena o conteúdo digitado dentro da string sobrenome
+		
+		file = fopen(arquivo,"a"); // abre o arquivo. O "a" significa que o arquivo será atualizado
+		fprintf(file,sobrenome); // escreve o conteúdo da variável sobrenome dentro do arquivo
+		fprintf(file,"| "); // escreve o conteúdo das aspas no arquivo
+		fclose(file); // fecha o arquivo
+		
+		printf("Digite o CARGO: "); //coletando informação do usuário
+		scanf("%s", cargo); // armazena o conteúdo digitado na string cargo
+		
+		file = fopen(arquivo,"a"); //abre o arquivo.
+		fprintf(file,"Cargo: "); //escreve o conteúdo das aspas dentro do arquivo
+		
+		fprintf(file,cargo); // escreve o conteúdo da variável cargo no arquivo
+		fclose(file); // fecha o arquivo
+		
+		printf("\n\n\n");
+		
+		printf( "\nOpções:\n");		
+		printf( "\tDigite ( 1 )- Para PESQUISAR outro CPF.\n");
+		printf( "\t\Digite outro número para Voltar ao MENU\n");
+				
+		printf("\t\nEscolha: ");
+				
+		scanf("%d", &selecao);
+		printf("%d", selecao);	
+			
+	}while (selecao ==1);
+		
 }
 
 int consulta() // consulta usuário cadastrados
-{
+{	
 	setlocale(LC_ALL,"Portuguese"); //Definindo a Linguagem
 	
 	//definindo variáveis
 	char cpf[40];
 	char conteudo[200];
-	
-	printf("### CONSULTA POR CPF ###\n\n\n");// título da tela
-	
-	printf("Digite o CPF: "); // coletando informação do usuário
-	scanf("%s",cpf); // armazena o conteúdo digitado dentro da variável cpf
+	int selecao = 1;
+	do{
+		system("cls");
+		printf("### CONSULTA POR CPF ###\n\n\n");// título da tela
 		
-	FILE *file; //chama o arquivo
-	file = fopen(cpf,"r"); // abre o arquivo cujo nome seja igual ao conteúdo da variável cpf. "r" read - le o arquivo
-	
-	if(file == NULL) // condicional: se o arquivo for nulo
-	{   // verdadeiro: exibe mensagem
-		printf("\nCPF não encontrado!\nVerifique o número digitado."); // exibe mensagem na tela
-		printf("\n\n\n");
-	}
-	
-	while (fgets(conteudo, 200,file) != NULL) // armazena até 200 caracteres na variável conteúdo, enquanto a condição for verdadeira ( diferente de Nulo)
-	{
-		printf("\nDados do Usuário:\n"); // exibe mensagema na tela
-		printf(conteudo); // exibe conteúdo do arquivo
-		printf("\n\n\n"); // pula 3 linhas
-	}
-	
-	system ("pause"); // pausa do sistema
-}
-
-int deletar() //deleta usuários cadastrados
-{
-	char cpf [40]; // declaração de variável
-		
-	printf("\t### DELETAR NOMES ###\n\n"); // título da tela
-	
-	printf(" Digite o CPF a ser DELETADO: "); // coletando informação do usuário
-	scanf("%s", cpf); // armazena o conteúdo da digitado na variável cpf
-	
-
-	FILE *file; // chama o arquivo
-	file = fopen (cpf, "r"); // abre o arquivo e lê seu conteúdo
-	fclose(file); // feche o arquivo
-	
-	
-	printf("__________________________________________\n\n"); // exibe um separador na tela
-	
-	if ( file == NULL) { // condicinal: se o arquivo for nula
-		
-		printf(">>> Este CPF  não existe no cadastro. <<<\n"); // verdadeiro: exibe mensagem na tela
-		
-		} else { // caso contrário; exibe mensagem na tela.( condição falsa )
-			remove(cpf); // remove da pasta o arquivo com nome igual ao conteúdo da variável cpf
-			printf(">>> CPF DELETADO com sucesso! <<<\n");
-		
-		}
-	printf("__________________________________________\n\n"); // exibe um separador na tela	
-	
-	
-	system("pause");  // pausa do sistema
+		printf("Digite o CPF: "); // coletando informação do usuário
+		scanf("%s",cpf); // armazena o conteúdo digitado dentro da variável cpf
 			
+		FILE *file; //chama o arquivo
+		file = fopen(cpf,"r"); // abre o arquivo cujo nome seja igual ao conteúdo da variável cpf. "r" read - le o arquivo
+		
+		if(file == NULL) // condicional: se o arquivo for nulo
+		{   // verdadeiro: exibe mensagem
+			printf("\nCPF não encontrado!\nVerifique o número digitado."); // exibe mensagem na tela
+			printf("\n\n\n");
+		}
+		
+		while (fgets(conteudo, 200,file) != NULL) // armazena até 200 caracteres na variável conteúdo, enquanto a condição for verdadeira ( diferente de Nulo)
+		{
+			printf("\nDados do Usuário:\n"); // exibe mensagema na tela
+			printf(conteudo); // exibe conteúdo do arquivo
+			printf("\n\n\n"); // pula 3 linhas
+		}
+		
+			printf( "\nOpções:\n");		
+			printf( "\tDigite ( 1 )- Para PESQUISAR outro CPF.\n");
+			printf( "\t\Digite outro número para Voltar ao MENU\n");
+				
+			printf("\t\nEscolha: ");
+				
+			scanf("%d", &selecao);
+			printf("%d", selecao);	
+			
+	}while (selecao ==1);
 }
+
+int deletar() {//deleta usuários cadastrados
+	
+	int selecao;
+	char cpf [40]; // declaração de variável	
+	
+	do{	
+		system ("cls");
+		printf("\t### DELETAR NOMES ###\n\n");
+		printf(" Digite o CPF a ser DELETADO: "); // coletando informação do usuário
+		scanf("%s", cpf); // armazena o conteúdo da digitado na variável cpf
+				
+		FILE *file; // chama o arquivo
+		file = fopen (cpf, "r"); // abre o arquivo e lê seu conteúdo
+		fclose(file); // feche o arquivo
+					
+		printf("__________________________________________\n\n"); // exibe um separador na tela
+			
+		if ( file == NULL) { // condicinal: se o arquivo for nula
+				
+			printf(">>> Este CPF  não existe no cadastro. <<<\n"); // verdadeiro: exibe mensagem na tela
+				
+			} else { // caso contrário; exibe mensagem na tela.( condição falsa )
+				remove(cpf); // remove da pasta o arquivo com nome igual ao conteúdo da variável cpf
+				printf(">>> CPF DELETADO com sucesso! <<<\n");
+			} // fim de if/else
+				
+		printf("__________________________________________\n\n"); // exibe um separador na tela	
+			
+		printf( "\nOpções:\n");		
+		printf( "\tDigite ( 1 )- Para informar outro CPF.\n");
+		printf( "\t\Digite outro número para Voltar ao MENU\n");
+			
+		printf("\t\nEscolha: ");
+			
+		scanf("%d", &selecao);
+		printf("%d", selecao);	
+			
+	}while (selecao ==1);
+
+} // fim da função deletar
 
 //INÍCIO DO PROGRAMA
 int main ()
@@ -139,11 +170,10 @@ int main ()
 	
 	for(laco=1;laco=1;)	// início da repetição ( neste caso sempre repete, pois a condição é sempre verdadeira)
 	{
-				
-		system("cls");	// limpa os dados da tela
-				
-		setlocale(LC_ALL,"Portuguese"); //Definindo a Linguagem
+		setlocale(LC_ALL,"Portuguese"); //Definindo a Linguagem		
 		
+		system("cls");	// limpa os dados da tela
+					
 		//Início do Menu
 		printf("\t### Cartório da EBAC ###\n\n");
 		printf("Escolha a opção desejada no menu:\n\n");
@@ -163,24 +193,27 @@ int main ()
 			registro();
 			break;
 			
-			case 2: // executa a função consulta se opcao=1
+			case 2: // executa a função consulta se opcao=2
 			consulta();
 			break;
 			
-			case 3: // executa a função deletar se opcao=1
+			case 3: // executa a função deletar se opcao=3
 			deletar();
 			break;
 				
 			default:// exibe a mensagem na tela quando a entrada não corresponde a nenhum valor da variável opcao
 			printf("Opção Indisponível\n\n"); 
 			system("pause");
+			main();
 			break;
+			
 			
 			// Fim da seleção
 		}
 		
-			
 	}
+
+	return 0;
 }
 
 
